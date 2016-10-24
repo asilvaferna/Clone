910e48aac7a8d4c75fcd26df4c36fe61fc90b550
Adrián Silva <adri4silva@gmail.com>
2016-10-24 09:06:13 +0200

    Commit

--- src/cd_clonados/CD_Clonados.java
+++ src/cd_clonados/CD_Clonados.java
@@ -3,6 +3,7 @@ public class CD_Clonados {
     public static void main(String[] args) {
         System.out.println("Esto es una prueba"); //Cambio1
         System.out.println("Sigue siendo una prueba"); //Cambio2
+        System.out.println("Otra prueba"); //Cambio3
     }
     
 }
