b9057211f89b7473c4b8ba5458290154778be68d
Adrián Silva <adri4silva@gmail.com>
2016-10-24 09:01:34 +0200

    Segunda prueba

--- src/cd_clonados/CD_Clonados.java
+++ src/cd_clonados/CD_Clonados.java
@@ -2,6 +2,7 @@ package cd_clonados;
 public class CD_Clonados {
     public static void main(String[] args) {
         System.out.println("Esto es una prueba"); //Cambio1
+        System.out.println("Sigue siendo una prueba"); //Cambio2
     }
     
 }
